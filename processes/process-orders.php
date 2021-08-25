4cea81bf31e09d2df3beb065194ecc00afe48e44
khalifa47 <khalifafumo5@gmail.com>
2021-08-25 13:02:57 +0300

    foodapp orders

--- processes/process-orders.php
+++ processes/process-orders.php
@@ -1,5 +1,6 @@
 <?php
 require_once("connect.php");
+require_once("time.php");
 
 createDatabase();
 createOrdersTable();
@@ -8,6 +9,19 @@ $sql_select = "SELECT cart.userID, cart.itemID, cart.quantity, cart.total_price,
                 INNER JOIN items ON items.itemID = cart.itemID
                 INNER JOIN users ON users.userID = cart.userID
                 INNER JOIN addresses ON addresses.userID = cart.userID";
-
-$sql_insert = "INSERT INTO Orders(userID, addressID, itemID, quantity, total_price, order_date, order_time, delivery_status)";
+if($rows = getData($sql_select)){
+    foreach($rows as $row){
+        $sql_insert = "INSERT INTO Orders(userID, addressID, itemID, quantity, total_price, order_date, order_time, delivery_status)
+        VALUES(" . $row["userID"] . ", " . $row["addressID"] . ", " . $row["itemID"] . ", " . $row["quantity"] . ", " . $row["total_price"] . ", '" . findDate() . "', '" . findTime() . "', 'Confirmed')";
+        if(!setData($sql_insert)){
+            echo setData($sql_insert);
+        }
+    }
+}
+else if($GLOBALS['isEmpty']){
+    echo "<script>alert('Please add items to the cart first!'); document.location = '../views/view-items.php';</script>";
+}
+else{
+    echo getData($sql_select);
+}
 ?>
