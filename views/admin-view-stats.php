408070f60e10c64bded1ad4601c6e760e266ddba
khalifa47 <khalifafumo5@gmail.com>
2021-09-02 22:29:42 +0300

    final update added charts

--- views/admin-view-stats.php
+++ views/admin-view-stats.php
@@ -15,6 +15,15 @@
     <?php require("../partials/nav.php"); ?>
     <?php require("../partials/verify.php"); ?>
 
+    <?php 
+    if(!isLogged()){
+        header("Location: login.php");
+    }
+    else if(!isAdmin()){
+        header("Location: index.php");
+    }
+    ?>
+
     <script>
         google.charts.load('current', {
             'packages': ['corechart']
@@ -23,38 +32,65 @@
 
         // Draw the chart and set the chart values
         function drawChart() {
-            const data = new google.visualization.DataTable();
-            data.addColumn('string', 'itemname');
-            data.addColumn('number', 'freq');
+            const data_items = new google.visualization.DataTable();
+            data_items.addColumn('string', 'itemname');
+            data_items.addColumn('number', 'freq');
+
+            const data_users = new google.visualization.DataTable();
+            data_users.addColumn('string', 'itemname');
+            data_users.addColumn('number', 'freq');
+
+
 
             <?php
-            $sql_select = "SELECT items.itemname, COUNT(*) AS freq FROM orders INNER JOIN items ON items.itemID = orders.itemID GROUP BY items.itemname";
-            if ($rows = getData($sql_select)) {
-                foreach ($rows as $row) {
-                    echo "data.addRow(['{$row['itemname']}', {$row['freq']}]);";
+            $sql_select_items = "SELECT items.itemname, COUNT(*) AS freq FROM orders INNER JOIN items ON items.itemID = orders.itemID GROUP BY items.itemname";
+            $sql_select_users = "SELECT users.username, COUNT(*) AS freq FROM orders INNER JOIN users ON users.userID = orders.userID GROUP BY users.username";
+            if ($rows_items = getData($sql_select_items)) {
+                foreach ($rows_items as $row_item) {
+                    echo "data_items.addRow(['{$row_item['itemname']}', {$row_item['freq']}]);";
                 }
             } else if ($GLOBALS['isEmpty']) {
                 echo "<h2 style='color: rgba(30, 0, 139, 0.75);'>Nothing to see here :(</h2>";
             } else {
-                echo getData($sql_select);
+                echo getData($sql_select_items);
+            }
+
+            if ($rows_users = getData($sql_select_users)) {
+                foreach ($rows_users as $row_user) {
+                    echo "data_users.addRow(['{$row_user['username']}', {$row_user['freq']}]);";
+                }
+            } else if ($GLOBALS['isEmpty']) {
+                echo "<h2 style='color: rgba(30, 0, 139, 0.75);'>Nothing to see here :(</h2>";
+            } else {
+                echo getData($sql_select_users);
             }
             ?>
 
-            // Optional; add a title and set the width and height of the chart
-            const options = {
+            const options_items = {
                 'title': 'Ordered Items',
                 'width': 550,
                 'height': 400
             };
 
-            // Display the chart inside the <div> element with id="piechart"
-            const chart = new google.visualization.PieChart(document.getElementById('piechart_ordered_items'));
-            chart.draw(data, options);
+            const options_users = {
+                'title': 'User order count',
+                'width': 550,
+                'height': 400
+            };
+
+            const chart_items = new google.visualization.PieChart(document.getElementById('piechart_ordered_items'));
+            const chart_users = new google.visualization.BarChart(document.getElementById('barchar_users_orders'));
+            chart_items.draw(data_items, options_items);
+            chart_users.draw(data_users, options_users);
         }
     </script>
 
     <h2>Stats</h2>
-    <div id="piechart_ordered_items"></div>
+    <div style="display: flex; justify-content:space-around">
+        <div id="piechart_ordered_items"></div>
+        <div id="barchar_users_orders"></div>
+    </div>
+    
 
     <?php require("../partials/footer.php"); ?>
 </body>
